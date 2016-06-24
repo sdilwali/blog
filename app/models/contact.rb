87e23fb5d72a6244a4bb9c5996064c045133f649
Sagar Dilwali <sagar.dilwali@gmail.com>
2016-06-24 23:28:57 +0000

    Added new contacts page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,3 @@
-#   Any data regarding the contact table goes here
 class Contact < ActiveRecord::Base
-    
-    
 
-
-   
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   # Generating a few routes... but all routes must be defined
   resources :contacts
-  
   get '/about' => 'pages#about' 
-  
-  
-  
-  resources :posts
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
