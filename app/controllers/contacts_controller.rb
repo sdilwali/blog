fbb77d5d7ee6debae5dc7a9ef4ba0fee79c2f17f
Sagar Dilwali <sagar.dilwali@gmail.com>
2016-06-25 00:42:31 +0000

    Added Validation for contact us

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,22 @@ class ContactsController < ApplicationController
     end
     
     def create
+        #create contact using whitelisted params
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] =  "Contact created successfully!"
+            redirect_to new_contact_path
+        else
+            flash[:warning] =  "Error, contact not created!"
+            redirect_to new_contact_path
+        end
+        
     end
+    
+    private 
+        def contact_params
+            # whitelist the paramets that are allowed
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
 
+# Add validation for Contact form
+    validates :name, presence: true
+    validates :email, presence: true
+    # validates :comments, presence: true
+
 end
