bb5d77e5cecfae3f0f932fac9fa7e56bf180b0c9
Sagar Dilwali <sagar.dilwali@gmail.com>
2016-06-30 08:02:57 +0000

    added stripe js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+    //watch for a form submission
+    $("#form-submit-btn").click(function(event) {
+        event.preventDefault();
+        $('input[type=submit]').prop('disabled', true);
+        var error = false;
+        var ccNum = $('#card_number').val(),
+            cvcNum = $('#card_code').val(),
+            expMon = $('#card_month').val(),
+            expYear = $('#card_year').val();
+            
+        if (!error) {
+            // get stripe token
+            Stripe.createToken({
+                number: ccNum,
+                cvc: cvcNum,
+                exp_month: expMon,
+                exp_year: expYear
+            }, stripeResponseHandler);
+        }
+        return false;
+    }); //form submission
+    
+    function stripeResponseHandler(status, response) {
+        //get a ref to the form
+        var f = $("#new_user");
+        
+        // get the token to the response:
+        var token = response.id;
+        
+        // add the token to the form:
+        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+        
+        f.get(0).submit();
+    }
+});
